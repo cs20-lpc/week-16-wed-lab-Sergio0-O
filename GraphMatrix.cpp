09a5b2e7c3479b3b51364727c3f47e684ad942fa
Sergio0-O <sergioostria166@gmail.com>
2026-05-09 21:32:39 -0700

    Update GraphMatrix.cpp

--- GraphMatrix.cpp
+++ GraphMatrix.cpp
@@ -1,15 +1,42 @@
 #include "GraphMatrix.hpp"
 
-GraphMatrix::GraphMatrix(int numV, GType type) {
+GraphMatrix::GraphMatrix(int numV, GType type) : Graph(numV, 0, type) {
     // TODO
+
+    // make the rows
+    adjMatrix = new bool*[v];
+
+    // make each row and fill with false
+    for (int row = 0; row < v; row++) {
+        adjMatrix[row] = new bool[v];
+
+        for (int col = 0; col < v; col++) {
+            adjMatrix[row][col] = false;
+        }
+    }
 }
 
 GraphMatrix::~GraphMatrix() {
     // TODO
+
+    // delete each row first
+    for (int row = 0; row < v; row++) {
+        delete[] adjMatrix[row];
+    }
+
+    // delete the row pointers
+    delete[] adjMatrix;
 }
 
 bool GraphMatrix::isEdge(int i, int j) const {
     // TODO
+
+    // check if vertices are valid
+    if (i < 0 || i >= v || j < 0 || j >= v) {
+        return false;
+    }
+
+    return adjMatrix[i][j];
 }
 
 ostream& GraphMatrix::print(ostream& os) const {
@@ -38,4 +65,25 @@ ostream& GraphMatrix::print(ostream& os) const {
 
 void GraphMatrix::toggleEdge(int i, int j) {
     // TODO
+
+    // check if vertices are valid
+    if (i < 0 || i >= v || j < 0 || j >= v) {
+        return;
+    }
+
+    bool addingEdge = !adjMatrix[i][j];
+
+    adjMatrix[i][j] = addingEdge;
+
+    // undirected graphs need both directions changed
+    if (graphType == UNDIRECTED) {
+        adjMatrix[j][i] = addingEdge;
+    }
+
+    if (addingEdge) {
+        e++;
+    }
+    else {
+        e--;
+    }
 }
